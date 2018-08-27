e77b1bfa3387bfcb3661723017d6331406607a64
Kirstin Ryan <ryan.kirstin@gmail.com>
2018-08-27 23:35:34 +0100

    visitor controller and vtl complete all classes except artists seeded comedian updated for nationality

--- festival_app/src/main/java/db/Seeds.java
+++ festival_app/src/main/java/db/Seeds.java
@@ -3,6 +3,7 @@ package db;
 import models.*;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Seeds {
 
@@ -11,24 +12,72 @@ public class Seeds {
 
     public static void seedData(){
 
+        DBHelper.deleteAll(Visitor.class);
         DBHelper.deleteAll(Venue.class);
         DBHelper.deleteAll(LineUp.class);
+        DBHelper.deleteAll(Performance.class);
         DBHelper.deleteAll(Artist.class);
         DBHelper.deleteAll(Musician.class);
 
-        Venue assembly = new Venue("Assembly George Square Gardens", "George Square, EH8 9LH", 10000);
-        DBHelper.save(assembly);
-        Venue bedlam = new Venue("Bedlam Theatre", "11b Bristo Place, EH1 1EZ", 5000);
-        DBHelper.save(bedlam);
-        Venue dancebase = new Venue("Dance Base", "14-16 Grassmarket, EH1 2JU", 2000);
-        DBHelper.save(dancebase);
-        Venue playhouse = new Venue("Edinburgh Playhouse", "18-22 Greenside Place, EH1 3AA", 10000);
-        DBHelper.save(playhouse);
-        Venue gildedballoon = new Venue("Gilded Balloon Teviot", "Teviot Row House, EH8 9AJ", 1000);
-        DBHelper.save(gildedballoon);
+        Visitor visitor1 = new Visitor("Digory", "Eyebrow");
+        DBHelper.save(visitor1);
+        Visitor visitor2 = new Visitor("Molly", "Drummer");
+        DBHelper.save(visitor2);
+        Visitor visitor3 = new Visitor("Vicky", "Jackson-Five");
+        DBHelper.save(visitor3);
+        Visitor visitor4 = new Visitor("Mike", "Thorpe-Park");
+        DBHelper.save(visitor4);
+        Visitor visitor5 = new Visitor("Robbie", "Drumbells");
+        DBHelper.save(visitor5);
+        Visitor visitor6 = new Visitor("Colin", "Riddles");
+        DBHelper.save(visitor6);
+        Visitor visitor7 = new Visitor("Ricardio", "Ruiz");
+        DBHelper.save(visitor7);
+        Visitor visitor8 = new Visitor("Stoo", "Black");
+        DBHelper.save(visitor8);
+        Visitor visitor9 = new Visitor("Gemma", "Prettigal");
+        DBHelper.save(visitor9);
+        Visitor visitor10 = new Visitor("Bee", "Ficek");
+        DBHelper.save(visitor10);
 
-        LineUp gildedballoon2608 = new LineUp(LocalDate.of(2018,8,24), gildedballoon);
-        DBHelper.save(gildedballoon2608);
+        Venue venue1 = new Venue("Assembly George Square Gardens", "George Square, EH8 9LH", 10000);
+        DBHelper.save(venue1);
+        Venue venue2 = new Venue("Bedlam Theatre", "11b Bristo Place, EH1 1EZ", 5000);
+        DBHelper.save(venue2);
+        Venue venue3 = new Venue("Dance Base", "14-16 Grassmarket, EH1 2JU", 2000);
+        DBHelper.save(venue3);
+        Venue venue4 = new Venue("Edinburgh Playhouse", "18-22 Greenside Place, EH1 3AA", 10000);
+        DBHelper.save(venue4);
+        Venue venue5 = new Venue("Gilded Balloon Teviot", "Teviot Row House, EH8 9AJ", 1000);
+        DBHelper.save(venue5);
+
+        LineUp lineUp1 = new LineUp(LocalDate.of(2018,8,24), venue1);
+        DBHelper.save(lineUp1);
+        LineUp lineUp2 = new LineUp(LocalDate.of(2018,8,25), venue2);
+        DBHelper.save(lineUp2);
+        LineUp lineUp3 = new LineUp(LocalDate.of(2018,8,26), venue3);
+        DBHelper.save(lineUp3);
+        LineUp lineUp4 = new LineUp(LocalDate.of(2018,8,24), venue4);
+        DBHelper.save(lineUp4);
+        LineUp lineUp5 = new LineUp(LocalDate.of(2018,8,25), venue5);
+        DBHelper.save(lineUp5);
+
+        Performance performance1 = new Performance(LocalTime.of(7,30), 60, lineUp1);
+        DBHelper.save(performance1);
+        Performance performance2 = new Performance(LocalTime.of(8,30), 60, lineUp1);
+        DBHelper.save(performance2);
+        Performance performance3 = new Performance(LocalTime.of(9,30), 30, lineUp1);
+        DBHelper.save(performance3);
+        Performance performance4 = new Performance(LocalTime.of(7,0), 60, lineUp3);
+        DBHelper.save(performance4);
+        Performance performance5 = new Performance(LocalTime.of(8,0), 60, lineUp3);
+        DBHelper.save(performance5);
+
+
+
+
+
+
 
         Musician bruno = new Musician("Bruno", "Mars", "Singer", "JayZ", MusicGenreType.POP);
         DBHelper.save(bruno);
--- festival_app/src/main/java/models/Comedian.java
+++ festival_app/src/main/java/models/Comedian.java
@@ -1,17 +1,29 @@
 package models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Locale;
 
 @Entity
 @Table(name = "comedians")
 public class Comedian extends Artist {
 
+    private Locale nationality;
+
     public Comedian() {
     }
 
-    public Comedian(String firstName, String lastName, String type, String manager) {
+    public Comedian(String firstName, String lastName, String type, String manager, Locale nationality) {
         super(firstName, lastName, type, manager);
+        this.nationality = nationality;
+    }
+
+    @Column(name = "nationality")
+    public Locale getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(Locale nationality) {
+        this.nationality = nationality;
     }
 
 }
